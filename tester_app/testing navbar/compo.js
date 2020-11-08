f70502485dfbb41754604972d8493ca78c59ee52
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-11-08 17:34:33 +0530

    Create compo.js

---
+++ tester_app/testing navbar/compo.js
@@ -0,0 +1,15 @@
+mport React from "react";
+//props
+import Nav from "./props/navigations.js"
+//avatar
+import Avatar from '@material-ui/core/Avatar';
+//icons
+import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone';
+import LocalHospitalTwoToneIcon from '@material-ui/icons/LocalHospitalTwoTone';
+import SupervisorAccountTwoToneIcon from '@material-ui/icons/SupervisorAccountTwoTone';
+import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
+import StorefrontTwoToneIcon from '@material-ui/icons/StorefrontTwoTone';
+import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
+import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
+import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
+import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
