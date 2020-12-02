963ed2f13ac483b1585e3dc7927ea3abbc5523ae
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-12-02 19:28:51 +0530

    dir-00

--- tester_app/testing navbar/compo.js
+++ tester_app/testing navbar/compo.js
@@ -18,7 +18,13 @@ import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone';
 import LocalHospitalTwoToneIcon from '@material-ui/icons/LocalHospitalTwoTone';
-
+import SupervisorAccountTwoToneIcon from '@material-ui/icons/SupervisorAccountTwoTone';
+import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
+import StorefrontTwoToneIcon from '@material-ui/icons/StorefrontTwoTone';
+import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
+import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
+import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
+import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 
 function Leftbar(){
   const [{ user }, dispatch] = useStateValue();
@@ -28,7 +34,7 @@ function Leftbar(){
     <Nav icon={<Avatar src={user.photoURL} />} name={user.displayName} />
     <Nav icon={<FlagTwoToneIcon />} name="Pages" />
     <Nav icon={<LocalHospitalTwoToneIcon />} name="COVID-19 Information Centre" />
-    
+
   </div>
  <hr className="line" />
   <div className="leftbar_botom">
