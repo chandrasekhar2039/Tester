0cacdff3d51c2918c7013fea067bee5986eefc44
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-12-02 19:27:53 +0530

    up-1

--- tester_app/testing navbar/compo.js
+++ tester_app/testing navbar/compo.js
@@ -18,13 +18,7 @@ import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone';
 import LocalHospitalTwoToneIcon from '@material-ui/icons/LocalHospitalTwoTone';
-import SupervisorAccountTwoToneIcon from '@material-ui/icons/SupervisorAccountTwoTone';
-import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
-import StorefrontTwoToneIcon from '@material-ui/icons/StorefrontTwoTone';
-import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
-import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
-import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
-import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
+
 
 function Leftbar(){
   const [{ user }, dispatch] = useStateValue();
