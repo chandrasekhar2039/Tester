de639d1327a6d95f089d587260b7039d1cefb220
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2020-12-04 21:20:07 +0530

    dw-2

--- tester_app/testing navbar/compo.js
+++ tester_app/testing navbar/compo.js
@@ -16,14 +16,7 @@ import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
 import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
 import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
-import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone';
-import LocalHospitalTwoToneIcon from '@material-ui/icons/LocalHospitalTwoTone';
-import SupervisorAccountTwoToneIcon from '@material-ui/icons/SupervisorAccountTwoTone';
-import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
-import StorefrontTwoToneIcon from '@material-ui/icons/StorefrontTwoTone';
-import VideoLibraryTwoToneIcon from '@material-ui/icons/VideoLibraryTwoTone';
-import EventAvailableTwoToneIcon from '@material-ui/icons/EventAvailableTwoTone';
-import UpdateTwoToneIcon from '@material-ui/icons/UpdateTwoTone';
+
 import KeyboardArrowDownRoundedIcon from '@material-ui/icons/KeyboardArrowDownRounded';
 
 function Leftbar(){
